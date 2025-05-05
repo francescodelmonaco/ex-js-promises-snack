e422f97280a81db0a3aa9de16608b6a2c76e7705
francescodelmonaco <francescodelmonaco1999@gmail.com>
2025-05-05 10:48:30 +0200

    add: snack 2

--- main.js
+++ main.js
@@ -13,3 +13,21 @@ function getPostTitle(id) {
 getPostTitle(22)
     .then(title => console.log(`Il titolo del post è: ${title}`))
     .catch(error => console.error(error))
+
+// snack 2
+function lanciaDado() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (Math.random < 0.2) {
+                reject(`Il dado si è incastrato... Rilancia!`);
+            } else {
+                const randomNum = Math.floor(Math.random() * 6) + 1;
+                resolve(randomNum);
+            }
+        }, 3000);
+    });
+};
+
+lanciaDado()
+    .then(num => console.log(`Il numero è il ${num}`))
+    .catch(err => console.error(err))
